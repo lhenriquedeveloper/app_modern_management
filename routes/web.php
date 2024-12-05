6dd2602a1a298f7c8117dbb0f26967d49593aa7c
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2024-12-05 10:29:31 -0300

    aula - 32

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/contato', [ContatoController::class, 'contato']);
