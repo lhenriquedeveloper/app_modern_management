f52d7c6ece82cd4bd55245c2dd08be3b52295193
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2024-12-13 11:23:52 -0300

    feat: aula 40

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
@@ -7,3 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 Route::get('/contato', [ContatoController::class, 'contato']);
+
+
+Route::get('/clientes', [PrincipalController::class, 'principal']);
+Route::get('/fornecedores', [SobreNosController::class, 'sobreNos']);
+Route::get('/produtos', [ContatoController::class, 'contato']);
+
